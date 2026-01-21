3c4cb5e017171a9833eba95ee9ce580154df7d23
Ramazanjiest <kalysbekovramazan7@gmail.com>
2026-01-21 14:16:19 +0600

    umar finish

--- myproject/house_app/admin.py
+++ myproject/house_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Property)
+admin.site.register(Region)
+admin.site.register(City)
+admin.site.register(District)
+admin.site.register(Review)
+admin.site.register(PropertyImg)
